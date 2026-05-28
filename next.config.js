686230267ff23f860aadb93865779aa2e61a4311
Chris <5566558+lamchr@users.noreply.github.com>
2026-05-28 12:57:51 -0400

    Fix custom domain asset paths

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const isUserOrOrgSite = repoName?.endsWith(".github.io");
-const basePath =
-  isGithubPages && repoName && !isUserOrOrgSite ? `/${repoName}` : "";
-
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined,
 };
 
 module.exports = nextConfig;
